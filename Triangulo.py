70e9d32fcdbb8ddb9ed8efc315f05f3d48b00a65
hgdky <>
2021-04-26 16:58:00 +0000

    Repositorio-Fundamentosprogramacion2

---
+++ Triangulo.py
@@ -0,0 +1,12 @@
+#Definicion de el triangulo.
+
+print ("Ejercicio de triangulo : Su area")
+
+b=int(input("ingrese base colega "))
+
+h=int(input("ingrese la altura colega "))
+
+
+area=(b*h)/2
+
+print("area del triangulo : ",  area)
